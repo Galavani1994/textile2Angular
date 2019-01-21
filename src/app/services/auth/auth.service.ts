1281df5bc05a5f5f618bf0f466ef9c52404cb21a
mahdi <mahdi.ghalavani@gmail.com>
2019-01-21 13:46:17 +0330

    added one directory

---
+++ src/app/services/auth/auth.service.ts
@@ -0,0 +1,25 @@
+import {Promise} from 'q';
+
+export class AuthService {
+    loggedIn = false;
+
+    isAuthenticate() {
+        const promise = new Promise(
+            (resolve, reject) => {
+                setTimeout(() => {
+                    resolve(this.loggedIn);
+                }, 800);
+            }
+        );
+
+        return promise;
+    }
+
+    login() {
+        this.loggedIn = true;
+    }
+
+    logout() {
+        this.loggedIn = false;
+    }
+}
